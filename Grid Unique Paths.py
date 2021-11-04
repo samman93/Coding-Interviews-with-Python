4dafdccc638b262a11578c2b29e48ad2ab168f89
Mohammad Samman Hossain <samman.hossain1@gmail.com>
2021-11-04 22:57:37 +0600

    Add files via upload

---
+++ Grid Unique Paths.py
@@ -0,0 +1,13 @@
+import math
+class Solution:
+	# @param A : integer
+	# @param B : integer
+	# @return an integer
+	def uniquePaths(self, A, B):
+        c = A - 1
+        d = B - 1
+
+		if(B == 1 or A == 1):
+			return 1
+
+        return int(math.factorial(c+d)/(math.factorial(c) * math.factorial(d)))
